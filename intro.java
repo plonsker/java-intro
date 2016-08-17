8a612941f5fa8feebbb6f304062e2db44c7ac80c
joe <joeplonsker@gmail.com>
2016-08-17 12:59:20 -0400

    Add all very early basic building blocks

---
+++ intro.java
@@ -0,0 +1,134 @@
+//Print your name
+
+public class YourName {
+  public static void main(String[] args) {
+
+    System.out.println("Joe");
+
+  }
+}
+
+// Print an integer
+
+public class DataTypes {
+  public static void main(String[] args) {
+
+    System.out.println(13);
+
+  }
+}
+
+// Print a boolean
+
+public class DataTypesB {
+  public static void main(String[] args) {
+
+    System.out.println(true);
+
+  }
+}
+
+// Print a char
+
+public class DataTypesC {
+  public static void main(String[] args) {
+
+    System.out.println('J');
+
+  }
+}
+
+// print a bunch of stuff!
+
+public class Variables {
+  public static void main(String[] args) {
+
+    int myNumber = 42;
+    boolean isFun = true; 
+    char movieRating = 'A'; 
+
+  }
+}
+
+// What do comments look like? Other than this line of course.
+
+public class Comments {
+  public static void main(String[] args) {
+
+    //System.out.println("Noise!");
+    
+    /*
+    
+    Sup
+    This
+    Is
+    Joe
+    */
+
+
+
+    
+  }
+}
+
+
+
+// Do some basic arithmtic
+
+public class Arithmetic {
+  public static void main(String[] args) {
+
+    int myNumber = 3 * 7;
+    System.out.println(myNumber);
+
+  }
+}
+
+// Modulo
+
+public class Modulo {
+  public static void main(String[] args) {
+
+    int myRemainder = 6 % 4;
+    System.out.println(myRemainder);
+
+  }
+} 
+
+// Relational stuff
+
+public class RelationalOperators {
+  public static void main(String[] args) {
+
+    System.out.println(7 > 13);
+
+  }
+}
+
+// Do some equality stuff
+
+public class EqualityOperators {
+  public static void main(String[] args) {
+    
+    char myLetter = 'J';
+    int myNumber = 11;
+    System.out.println(myLetter == myNumber);
+
+  }
+}
+
+// A few more examples
+
+public class Generalizations {
+  public static void main(String[] args) {
+
+    //joe was here
+
+    boolean isComplete = true;
+    int awesomeLevel = 121;
+    int epicLevel = awesomeLevel * 2;   
+    System.out.println(epicLevel);
+    
+
+  }
+}
